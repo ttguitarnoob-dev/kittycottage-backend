22b76750bb3ac585f1f2fdc0a058199f8dfb991e
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-06-28 09:54:24 -0500

    routes stubbed out and tested

--- server.js
+++ server.js
@@ -24,5 +24,29 @@ app.get('/items', (req, res) => {
     res.send("hello from the index route")
 })
 
+//New Item
+app.get('/items/new', (req, res) => {
+    console.log('new item')
+    res.send('Hello from new item route')
+})
+
+//Create Route
+app.post('/items', (req, res) => {
+    console.log('create items')
+    res.send('Hello from creating')
+})
+
+//Delete Route
+app.delete('/items/:id', (req, res) => {
+    console.log('delete route')
+    res.send('hello from delete route')
+})
+
+//Edit Route
+app.put('/items/:id', (req, res) => {
+    console.log('edit route')
+    res.send('Hello from the edit route')
+})
+
 //StartServer
 app.listen(PORT, () => console.log('App listening on port ', PORT))
