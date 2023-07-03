452ef170d431f2beeda6e6933c008a6f43dcb0f6
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-07-03 14:09:17 -0500

    export todo schema

--- models/todo.js
+++ models/todo.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose')
 
 const todoSchema = new mongoose.Schema({
+
     item: {
         type: String,
         require: true, default: "Pet your husband"
     },
+    
     completed: {
         type: Boolean,
         default: false
     }
 })
+
+const Todo = mongoose.model("Todo", todoSchema)
+module.exports = Todo
