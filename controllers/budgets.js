e477c9411631150aaf641fff9bca776ad3b042a2
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2024-02-17 10:21:32 -0600

    index route data handling for budgets route

--- controllers/budgets.js
+++ controllers/budgets.js
@@ -5,9 +5,21 @@ const router = express.Router()
 //Index
 router.get('/', async (req, res) => {
     try {
+        let parsedBudget = []
         const allBudgets = await Budget.find()
-        console.log('hello from index route', allBudgets)
-        res.json(allBudgets)
+        allBudgets.map((oneBudget) =>{
+            console.log("OMG", oneBudget.month, oneBudget.unpaid, oneBudget._id)
+            parsedBudget.push(
+                {
+                    key: oneBudget._id,
+                    month: oneBudget.month,
+                    unpaid: oneBudget.unpaid
+                }
+            )
+        })
+        // console.log('hello from index route', allBudgets)
+        console.log("apaprps", parsedBudget)
+        res.json(parsedBudget)
     } catch(err) {
         res.send("index route error", err)
     }
